e4341f158fd646ecd7efa50a6f9aa3dbaee88f50
Michael Gauthier <mike@silverorange.com>
2005-07-06 15:08:11 +0000

    Put headers into their own private method.
    git-svn-id: file:///Users/gauthierm/svnrepo/sajax/trunk@1032 94490950-a86c-4493-9ccb-530f72933e47

--- Sajax.php
+++ Sajax.php
@@ -186,15 +186,9 @@ class HTTP_Sajax
 			return false;
 		}
 
-		if ($mode == HTTP_SAJAX_TYPE_GET) {
+        $this->_sendHeaders();
 
-			// Disable any caching with HTTP headers
-			// Any date in the past will do here
-			header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-			header ('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-			// Always modified
-			header ('Cache-Control: no-cache, must-revalidate');  // HTTP/1.1
-			header ('Pragma: no-cache');                          // HTTP/1.0
+		if ($mode == HTTP_SAJAX_TYPE_GET) {
 
 			$function_name = $_GET['rs'];
 			if (isset($_GET['rsargs'])) {
@@ -227,6 +221,30 @@ class HTTP_Sajax
 	}
 
 	// }}}
+    // {{{ private function _sendHeaders()
+
+    /**
+     * Sends headers disabling any form of caching and notifying the client
+     * that is it receiving XML
+     */
+    private function _sendHeaders()
+    {
+        // Set content type to XML
+        header('Content-type: text/xml');
+
+		// Disable any caching with HTTP headers
+		// Any date in the past will do here
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+		header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+        
+		// Set always modified
+        // for HTTP/1.1
+		header('Cache-Control: no-cache, must-revalidate max-age=0');
+        // for HTTP/1.0
+		header('Pragma: no-cache');
+    }
+    
+    // }}}
 	// {{{ private fucntion _getDebugJavascript()
 	
 	/**
