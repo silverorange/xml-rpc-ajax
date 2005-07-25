2b3beddaaedfa166fa5a15ab1dbdb94ccb2c913c
Michael Gauthier <mike@silverorange.com>
2005-07-25 19:23:13 +0000

    Added a php class by Nick that displays the appropriate javascript.
    git-svn-id: file:///Users/gauthierm/svnrepo/xml-rpc-ajax/trunk@1169 94490950-a86c-4493-9ccb-530f72933e47

---
+++ XMLRPCAjax.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The initial setup of an XML-RPC Ajax component
+ *
+ * @package   xml-rpc-ajax
+ * @copyright 2005 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+class XMLRPCAjax
+{
+	public function display()
+	{
+		?>
+		<script src="xml-rpc-ajax/javascript/XML_RPC_Types.js" type="text/javascript"></script>
+		<script src="xml-rpc-ajax/javascript/XML_RPC_Exception.js" type="text/javascript"></script>
+		<script src="xml-rpc-ajax/javascript/XML_RPC_Request.js" type="text/javascript"></script>
+		<script src="xml-rpc-ajax/javascript/XML_RPC_Response.js" type="text/javascript"></script>
+		<script src="xml-rpc-ajax/javascript/XML_RPC_Client.js" type="text/javascript"></script>
+		<?php
+	}
+}
+
+?>
