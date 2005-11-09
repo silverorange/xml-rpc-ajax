5a419465b12fa4cb54d1b8f3208c83728c92ea26
David Peck <dave@silverorange.com>
2005-11-09 16:00:27 +0000

    php script to build our pear spec file, remove the manually built one
    git-svn-id: file:///Users/gauthierm/svnrepo/xml-rpc-ajax/trunk@2483 94490950-a86c-4493-9ccb-530f72933e47

---
+++ package.php
@@ -0,0 +1,67 @@
+<?php
+
+require_once 'PEAR/PackageFileManager2.php';
+
+$version = '0.0.4';
+$notes = <<<EOT
+- initial test package
+EOT;
+
+$description =<<<EOT
+This class provides an object oriented interface for
+asynchronous server side PHP calls from client side
+javascript without the need for a full page request.
+EOT;
+
+$package = new PEAR_PackageFileManager2();
+PEAR::setErrorHandling(PEAR_ERROR_DIE);
+
+$result = $package->setOptions(
+	array(
+		'filelistgenerator' => 'svn',
+		'simpleoutput'      => true,
+		'baseinstalldir'    => '/XML/RPC',
+		'packagedirectory'  => './',
+		'dir_roles'         => array(
+			'www' => 'data',
+			'tests' => 'test'
+		),
+		'exceptions'        => array(
+			'README' => 'doc'
+		),
+	)
+);
+
+$package->setPackage('XML_RPCAjax');
+$package->setSummary('XML-RPC client implementation using AJAX');
+$package->setDescription($description);
+$package->setChannel('pear.silverorange.com');
+$package->setPackageType('php');
+$package->setLicense('LGPL', 'http://www.gnu.org/copyleft/lesser.html');
+
+$package->setReleaseVersion($version);
+$package->setReleaseStability('beta');
+$package->setAPIVersion('0.0.1');
+$package->setAPIStability('beta');
+$package->setNotes($notes);
+
+$package->addIgnore('package.php');
+$package->addIgnore('deprecated');
+
+$package->addMaintainer('lead', 'nrf', 'Nathan Fredrickson', 'nathan@silverorange.com');
+$package->addMaintainer('lead', 'gauthierm', 'Mike Gauthier', 'mike@silverorange.com');
+
+$package->setPhpDep('5.0.5');
+$package->setPearinstallerDep('1.4.0');
+$package->addPackageDepWithChannel('required', 'XML_RPC2', 'pear.silverorange.com', '0.0.1');
+$package->generateContents();
+
+//$package->addReplacement('package-info', 'pear-config', '@package_version@', 'version');
+
+if (isset($_GET['make']) || (isset($_SERVER['argv']) && @$_SERVER['argv'][1] == 'make')) {
+	$package->writePackageFile();
+} else {
+	$package->debugPackageFile();
+}
+
+?>
