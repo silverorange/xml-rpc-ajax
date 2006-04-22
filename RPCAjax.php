4484bfd5ad76d869105c7c6646f66e384444bcbe
Michael Gauthier <mike@silverorange.com>
2006-04-22 22:01:08 +0000

    Added static method to get an array of html head entries for the xml-rpc ajax client.
    git-svn-id: file:///Users/gauthierm/svnrepo/xml-rpc-ajax/trunk@4853 94490950-a86c-4493-9ccb-530f72933e47

--- RPCAjax.php
+++ RPCAjax.php
@@ -1,10 +1,12 @@
 <?php
 
+require_once 'Swat/SwatHtmlHeadEntry.php';
+
 /**
  * The initial setup of an XML-RPC Ajax component
  *
  * @package   xml-rpc-ajax
- * @copyright 2005 silverorange
+ * @copyright 2005-2006 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class XML_RPCAjax
@@ -18,6 +20,40 @@ class XML_RPCAjax
 		<script src="xml-rpc-ajax/javascript/XML_RPC_Response.js" type="text/javascript"></script>
 		<script src="xml-rpc-ajax/javascript/XML_RPC_Client.js" type="text/javascript"></script>
 		<?php
+	}
+
+	/**
+	 * Gets the HTML head entries required by this AJAX XML-RPC client
+	 *
+	 * @return array a reference to an array of SwatHtmlHeadEntry objects
+	 *                containing the required JavaScript files for this AJAX
+	 *                XML-RPC client.
+	 */
+	public static function &getHtmlHeadEntries()
+	{
+		$entries = array();
+		$entries['xml-rpc-ajax/javascript/XML_RPC_Types.js'] =
+			new SwatHtmlHeadEntry('xml-rpc-ajax/javascript/XML_RPC_Types.js',
+				SwatHtmlHeadEntry::TYPE_JAVASCRIPT);
+
+		$entries['xml-rpc-ajax/javascript/XML_RPC_Exception.js'] =
+			new SwatHtmlHeadEntry(
+				'xml-rpc-ajax/javascript/XML_RPC_Exception.js',
+				SwatHtmlHeadEntry::TYPE_JAVASCRIPT);
+
+		$entries['xml-rpc-ajax/javascript/XML_RPC_Request.js'] =
+			new SwatHtmlHeadEntry('xml-rpc-ajax/javascript/XML_RPC_Request.js',
+				SwatHtmlHeadEntry::TYPE_JAVASCRIPT);
+
+		$entries['xml-rpc-ajax/javascript/XML_RPC_Response.js'] =
+			new SwatHtmlHeadEntry('xml-rpc-ajax/javascript/XML_RPC_Response.js',
+				SwatHtmlHeadEntry::TYPE_JAVASCRIPT);
+
+		$entries['xml-rpc-ajax/javascript/XML_RPC_Client.js'] =
+			new SwatHtmlHeadEntry('xml-rpc-ajax/javascript/XML_RPC_Client.js',
+				SwatHtmlHeadEntry::TYPE_JAVASCRIPT);
+
+		return $entries;
 	}
 }
 
