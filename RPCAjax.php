670dd6c8abd2191bbc3fbbbca59d7d41d1fc151d
Michael Gauthier <mike@silverorange.com>
2006-10-22 06:32:44 +0000

    remove unused and obsoleted method
    git-svn-id: file:///Users/gauthierm/svnrepo/xml-rpc-ajax/trunk@12214 94490950-a86c-4493-9ccb-530f72933e47

--- RPCAjax.php
+++ RPCAjax.php
@@ -17,17 +17,6 @@ class XML_RPCAjax
 	 */
 	const PACKAGE_ID = 'XML_RPCAjax';
 
-	public function display()
-	{
-		?>
-		<script src="xml-rpc-ajax/javascript/XML_RPC_Types.js" type="text/javascript"></script>
-		<script src="xml-rpc-ajax/javascript/XML_RPC_Exception.js" type="text/javascript"></script>
-		<script src="xml-rpc-ajax/javascript/XML_RPC_Request.js" type="text/javascript"></script>
-		<script src="xml-rpc-ajax/javascript/XML_RPC_Response.js" type="text/javascript"></script>
-		<script src="xml-rpc-ajax/javascript/XML_RPC_Client.js" type="text/javascript"></script>
-		<?php
-	}
-
 	/**
 	 * Gets the HTML head entries required by this AJAX XML-RPC client
 	 *
