d2ceb221634bff273c64ef74c41141921293a2ea
Colin Viebrock <colin@viebrock.ca>
2024-09-19 13:11:40 -0500

    Add tooling

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+};
